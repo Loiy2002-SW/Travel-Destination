4860bdc6a86a6f60c1ac711ceb17ecbed80052ea
Loiy2002-SW <lloiy9310@gmail.com>
2024-05-08 21:46:09 +0000

    fix issue in lab 17

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 
 
-export default function Navbar(prop){
+export default function Navbar() {
 
-    return(
-        <nav>
-        <button><Link to={'/'}>Home</Link></button>
-        <button><Link to={`/city/:${prop.id}`}>Details</Link></button>
-        </nav>
+    return (
+        <Link to={'/'}>
+            <button className='HomeButton'>Home</button>
+        </Link>
     );
 
 }
